7f1474868283e003b0a39b1a0b377ac923f63cf4
Dzikry Maulana <128385435+dkpage@users.noreply.github.com>
2023-04-05 03:43:34 +0700

    Update cover.php

--- cover.php
+++ cover.php
@@ -7,26 +7,39 @@
                 <h2 class="judul2"><strong><?=$sekolah?></strong></h2>
             </div>
             <div class="text-center mb-3 d-grid gap-2 m-3 p-3 btn-main justify-content-center">
-                <button class=" btn btn-primary btn-lg  p-2" role="button" id="tbMain"> <i
-                        class="fa-solid fa-qrcode mr-3"></i>Isi Absen</button>
-                <div class="" id="tbSecon" style="display:none;">
+                <button class=" btn btn-primary btn-lg  py-2 d-flex justify-content-between align-items-center px-3"
+                    role="button" id="tbMain">
+                    <i class="fa-solid fa-qrcode mr-3"></i>
+                    Absen Sekarang
+                    <i class="fa fa-angle-down"></i>
+                </button>
+                <div class="tutup" id="tbSecon">
                     <div class="d-grid gap-2">
-                        <button class=" btn btn-success " role="button" id="tbAbsen">Reguler</button>
-                        <button class=" btn btn-secondary  " role="button" id="tbInvaler">Invaler</button>
+                        <button class=" btn btn-outline-primary" role="button" id="tbAbsen">Reguler</button>
+                        <button class=" btn btn-outline-dark" role="button" id="tbInvaler">Invaler</button>
                     </div>
 
                     <!-- <a href="login" class="btn btn-outline-primary btn-lg p-2" role="button"><i
                             class="fa-solid fa-user mr-2"></i> User Panel</a> -->
                 </div>
-                <a href="login" class="btn btn-outline-primary btn-lg p-2" id="tbLogin"><i
-                        class="fa-solid fa-user mr-2"></i> User Panel</a>
+                <a href="login"
+                    class="btn btn-outline-primary btn-lg py-2 d-flex justify-content-between align-items-center px-3"
+                    id="tbLogin">
+                    <i class="fa-solid fa-user mr-2"></i>
+                    User Panel
+                    <i class="fa fa-angle-right"></i>
+                </a>
             </div>
 
         </div>
         <br>
         <br>
         <span class="mt-5 fixed-bottom mb-5">
-            <p class="text-disabled text-center copyright">© <?php echo $thnid.' - '.$sekolah?></p>
+            <p class="text-disabled text-center">&copy; <?php echo $thnid.' - '.$sekolah?><br> Dibuat dan dikembangkan
+                oleh
+                <a href="https://s.id/dzikry-maulana" target="_blank" rel="noopener noreferrer">Dzikry
+                    Maulana</a>
+            </p>
         </span>
     </div>
 </section>
@@ -40,18 +53,11 @@ if (!/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navig
 </div>
 
 <script>
-let klik = 0;
+// let klik = 0;
 $("#tbMain").on("click", function() {
-    if (klik == 0) {
-        $("#tbSecon").show();
-        klik = 1
-        // console.log("klik ke " + klik);
-    } else if (klik == 1) {
-        $("#tbSecon").hide();
-        klik = 0;
-    }
-    // document.getElementById("tbSecon").hidden = false;
-    // $("#tbMain").hide();
+    $("#tbSecon").toggleClass("tutup");
+    $("#tbLogin").toggleClass("tutup");
+    $("#tbMain > .fa-angle-down").toggleClass("rtt");
 })
 $("#tbClose").on("click", function() {
     document.getElementById("tbSecon").hidden = true;
